362501e09f6485ddda8fed0dcf626292c9ed7c91
Vedataliu <120605263+Vedataliu@users.noreply.github.com>
2024-01-31 23:51:36 +0100

    Update log&reg.php
    Përditësime teksuale rreth gjuhës!

--- logical view/log&reg.php
+++ logical view/log&reg.php
@@ -37,22 +37,22 @@ if(isset($_SESSION['email'])){
         <div class="front">
           <img src="images/frontImg.jpg" alt="">
           <div class="text">
-            <span class="text-1">You just write and <br> and we speak for you!</span>
-            <span class="text-2">Let's get connected</span>
+            <span class="text-1">Ju vetëm shkruani <br> dhe ne flasim për ju!</span>
+            <span class="text-2">Le të lidhemi!</span>
           </div>
         </div>
         <div class="back">
           <img class="backImg" src="images/backImg.jpg" alt="">
           <div class="text">
-            <span class="text-11">Start miles of journeys <br> with one step</span>
-            <span class="text-22">Let's get started</span>
+            <span class="text-11">Nis udhëtimin e kënaqësisë<br> me një hap</span>
+            <span class="text-22">Le të fillojmë</span>
           </div>
         </div>
       </div>
       <div class="forms">
         <div class="form-content">
           <div class="login-form">
-            <div class="title">Login</div>
+            <div class="title">Kyqu</div>
             <form name="loginForm" action="DB&OOP/loginDB.php" onsubmit="return validateLoginForm()" method="POST">
               <div class="input-boxes">
                 <div class="input-box">
@@ -63,34 +63,34 @@ if(isset($_SESSION['email'])){
                   <i class="fas fa-lock"></i>
                   <input type="password" name="password" placeholder="Enter your password" required>
                 </div>
-                <div class="text"><a href="#">Forgot password?</a></div>
+                <div class="text"><a href="#">Keni harruar fjalëkalimin?</a></div>
                 <div class="button input-box">
                   <input type="submit" name="loginBtn" value="Sumbit">
                 </div>
-                <div class="text sign-up-text">Don't have an account? <label for="flip">Sigup now</label></div>
+                <div class="text sign-up-text">Nuk keni llogari? <label for="flip">Regjistrohuni tani</label></div>
               </div>
             </form>
           </div>
           <div class="signup-form">
-            <div class="title">Signup</div>
+            <div class="title">Regjistrohuni</div>
             <form name="signupForm" action="DB&OOP/controller/registerController.php" onsubmit="return validateSignupForm()" method="POST">
               <div class="input-boxes">
                 <div class="input-box">
                   <i class="fas fa-user"></i>
-                  <input type="text" name="name" placeholder="Enter your name" required>
+                  <input type="text" name="name" placeholder="Shëno emrin tuaj" required>
                 </div>
                 <div class="input-box">
                   <i class="fas fa-envelope"></i>
-                  <input type="text" name="email" placeholder="Enter your email" required>
+                  <input type="text" name="email" placeholder="Shëno emailin tuaj" required>
                 </div>
                 <div class="input-box">
                   <i class="fas fa-lock"></i>
-                  <input type="password" name="password" placeholder="Enter your password" required>
+                  <input type="password" name="password" placeholder="Shëno passwordin tuaj" required>
                 </div>
                 <div class="button input-box">
-                  <input type="submit" name="registerBtn" value="register">
+                  <input type="submit" name="registerBtn" value="regjistrohu">
                 </div>
-                <div class="text sign-up-text">Already have an account? <label for="flip">Login now</label></div>
+                <div class="text sign-up-text">Keni llogari të hapur? <label for="flip">Kyquni tani</label></div>
               </div>
             </form>
           </div>
